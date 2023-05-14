31ec6c71785d6e4b80e09406caaad9051d88e547
Jhon Brayl Malinao <jhonbraylclear@gmail.com>
2023-05-14 11:28:02 +0800

    Add files via upload

---
+++ SariSariStore.cs
@@ -0,0 +1,28 @@
+﻿namespace Store_It_App
+{
+    public class SariSariStore
+    {
+        public List<Customer> Customers { get; set; }
+        public List<Sale> Sales { get; set; }
+
+        public SariSariStore()
+        {
+            Customers = new List<Customer>();
+            Sales = new List<Sale>();
+        }
+
+        public void AddCustomer(Customer customer)
+        {
+            Customers.Add(customer);
+        }
+
+        public void RecordSale(Sale sale)
+        {
+            Sales.Add(sale);
+        }
+
+        public void RecordDebtPayment(Customer customer, decimal amount)
+        {
+            customer.Debt -= amount;
+        }
+    }
