dd441a9306f412b70bcb1defb1ddb10ffe9b3421
Jhon Brayl Malinao <jhonbraylclear@gmail.com>
2023-05-20 23:25:04 +0800

    Update Transaction.cs

--- Transaction.cs
+++ Transaction.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace store
 {
-    internal class Class1
+    public class Transaction
+
+{
+
+    public Customer Customer { get; set; }
+
+    public TransactionType Type { get; set; }
+
+    public decimal Amount { get; set; }
+
+    public Transaction(Customer customer, TransactionType type, decimal amount)
+
     {
+
+        Customer = customer;
+
+        Type = type;
+
+        Amount = amount;
+
     }
+
+}
 }
