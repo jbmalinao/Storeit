8baeb4554dfb3544ea09f6d3085360bd6e1e3726
Jhon Brayl Malinao <jhonbraylclear@gmail.com>
2023-05-20 23:14:37 +0800

    Update Sale.cs

--- Sale.cs
+++ Sale.cs
@@ -1,7 +1,39 @@
-﻿namespace store
+using System;
+
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace store
 {
-    public class Class1
+    public class Sale
+    {
+       public DateTime DateTime { get; set; }
+
+       public List<SaleItem> Items { get; set; }
+
+       public decimal TotalAmount { get; set; }
+
+       public decimal Profit { get; set; }
+
+      public Sale()
+
     {
 
+        DateTime = DateTime.Now;
+
+        Items = new List<SaleItem>();
+
+        TotalAmount = 0;
+
+        Profit = 0;
+
+    }
     }
 }
